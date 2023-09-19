56817c32d4be96f6760eb3156e68c482caabed71
Oganes Grigoryan <og.gr1goryan@yandex.ru>
2023-09-19 17:17:41 +0300

    fix auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -11,7 +13,7 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'SECRET_KEY'); // HARDCODE SECRET_KEY
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'SECRET_KEY');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
